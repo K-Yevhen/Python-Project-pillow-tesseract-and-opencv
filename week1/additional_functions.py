cdea99fcdff11d8d8f613a5f691cfe72d167050b
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-08-14 13:47:05 +0200

    updated file
    testing with enhancing

--- week1/additional_functions.py
+++ week1/additional_functions.py
@@ -13,3 +13,27 @@ for i in range(0, 10):
 # print(images)
 
 first_image = images[0]
+contact_sheet = Image.new(first_image.mode, (first_image.width, 10*first_image.height))
+
+current_location = 0
+for img in images:
+    contact_sheet.paste(img, (0, current_location))
+    current_location = current_location + 450
+
+contact_sheet = contact_sheet.resize((160, 900))
+# contact_sheet.show()
+
+contact_sheet_new = Image.new(first_image.mode, (first_image.width*3, first_image.height*3))
+x = 0
+y = 0
+
+for img in images[1:]:
+    contact_sheet_new.paste(img, (x, y))
+    if x + first_image.width == contact_sheet_new.width:
+        x = 0
+        y = y + first_image.height
+    else:
+        x = x + first_image.width
+
+contact_sheet_new = contact_sheet_new.resize((int(contact_sheet_new.width/2), int(contact_sheet_new.height/2)))
+contact_sheet_new.show()
