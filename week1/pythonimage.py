8f9641927c8bbcbeb31bdf6ac775341583daec3d
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-08-14 11:32:54 +0200

    finished with basic functions of the pillow

--- week1/pythonimage.py
+++ week1/pythonimage.py
@@ -8,7 +8,7 @@ from IPython.display import display
 # print(dir(PIL))
 
 
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageDraw
 
 # print(help(Image))
 # print(help(ImageFilter))
@@ -33,5 +33,22 @@ image = Image.open(file)
 image = image.convert('RGB')  # this stands for red, green blue mode
 blurred_image = image.filter(ImageFilter.BLUR)
 # display(blurred_image)
-blurred_image.show()
-blurred_image.save("blurred.jpg")
+# blurred_image.show()
+# blurred_image.save("blurred.jpg")
+
+
+# box_filter = image.filter(ImageFilter.BoxBlur(5))
+# box_filter = image.filter(ImageFilter.BoxBlur(10))
+# box_filter.show()
+#
+# gaussian_filter = image.filter(ImageFilter.GaussianBlur)
+# gaussian_filter.show()
+
+# print("{}x{}".format(image.width, image.height))
+
+logo_image = image.crop((50, 0, 190, 150))
+# logo_image.show()
+
+drawing_object = ImageDraw.Draw(image)
+drawing_object.rectangle((50, 0, 190, 150), fill=None, outline='red')
+image.show()
