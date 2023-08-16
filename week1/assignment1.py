05dc607c0b16b3e64198663faa6d4e1f079f0fd1
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-08-16 11:53:30 +0200

    finished assignment 1
    added .py, notebook and .html files

--- week1/assignment1.py
+++ week1/assignment1.py
@@ -1,5 +1,5 @@
 import PIL
-from PIL import Image, ImageEnhance, ImageDraw
+from PIL import Image, ImageEnhance, ImageDraw, ImageFont
 from IPython.display import display
 
 # read image and convert to RGB
@@ -7,62 +7,38 @@ image = Image.open("msi_recruitment.gif")
 image = image.convert('RGB')
 
 # build a list of 9 images which have different color
+# for i in range(1, 10):
+#     images.append(image)
+
 images = []
-for i in range(1, 10):
-    images.append(image)
+labels = []
+for segami in range(3):
+    for intensity in (0.1, 0.5, 0.9):
+        splited_image = image.split()
+        mid = splited_image[segami].point(lambda x: x*intensity)
+        splited_image[segami].paste(mid)
+        im = Image.merge(image.mode, splited_image)
+        labels.append('channel {} intensity {}'.format(segami, intensity))
+        images.append(im)
+font = ImageFont.truetype("fanwood-webfont.ttf", 75)
+
 
 # create a contact sheet from different brightnesses
 first_image = images[0]
 contact_sheet = PIL.Image.new(first_image.mode, (first_image.width * 3, first_image.height * 3))
-x1 = 0
-y1 = 0
+x = 0
+y = 0
 
-row1 = []
-row2 = []
-row3 = []
-intensity = .1
-width, height = image.size
-
-for img in images[0:3]:
-    for y in range(height):
-        for x in range(width):
-            current_color1 = img.getpixel((x, y))
-            r1, g1, b1 = current_color1
-            updated = (r1) * intensity
-            new_color1 = ((int(updated)), (int(g1)), (int(b1)))
-            img.putpixel((x, y), new_color1)
-    intensity = intensity + .4
-    row1.append(img)
-
-for img in images[3:6]:
-    for y in range(height):
-        for x in range(width):
-            current_color2 = img.getpixel((x, y))
-            r2, g2, b2 = current_color2
-            new_color2 = ((int(r2)), (int(g2 * intensity)), (int(b2)))
-            img.putpixel((x, y), new_color2)
-    intensity = intensity + .4
-    row2.append(img)
-
-for img in images[6:]:
-    for y in range(height):
-        for x in range(width):
-            current_color3 = img.getpixel((x, y))
-            r3, g3, b3 = current_color3
-            new_color3 = ((int(r3)), (int(g3)), (int(b3 * intensity)))
-            img.putpixel((x, y), new_color3)
-    intensity = intensity + .4
-    row3.append(img)
-
-finallist = row1 + row2 + row3
-
-for img in finallist:
-    contact_sheet.paste(img, (x1, y1))
-    if x1 + first_image.width == contact_sheet.width:
-        x1 = 0
-        y1 = y1 + first_image.height
+for img in images:
+    # Lets paste the current image into the contact sheet
+    contact_sheet.paste(img, (x, y))
+    # Now we update our X position. If it is going to be the width of the image, then we set it to 0
+    # and update Y as well to point to the next "line" of the contact sheet.
+    if x+first_image.width == contact_sheet.width:
+        x = 0
+        y = y+first_image.height
     else:
-        x1 = x1 + first_image.width
+        x = x+first_image.width
 
 contact_sheet = contact_sheet.resize((int(contact_sheet.width / 2), int(contact_sheet.height / 2)))
 contact_sheet.show()
