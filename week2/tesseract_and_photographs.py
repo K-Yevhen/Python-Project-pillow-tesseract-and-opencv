108be14c7f56138525650861172dd810e6407d23
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-09-05 13:39:08 +0200

    materials to lessons 3

---
+++ week2/tesseract_and_photographs.py
@@ -0,0 +1,10 @@
+# Lets try a new example and bring together some of the things we have learned.
+# Here's an image of a storefront, lets load it and try and get the name of the
+# store out of the image
+from PIL import Image
+import pytesseract
+# Lets read in the storefront image I've loaded into the course and display it
+image=Image.open('storefront.jpg')
+image.show()
+# Finally, lets try and run tesseract on that image and see what the results are
+pytesseract.image_to_string(image)
