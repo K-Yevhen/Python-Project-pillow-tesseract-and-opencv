4c26affdb24001d994bac64b12558f1a65788c97
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-09-05 12:51:56 +0200

    updated

--- week2/module2.py
+++ week2/module2.py
@@ -6,10 +6,53 @@ from PIL import Image
 
 image = Image.open("text.png")
 # display(image)
-image.show()
+# image.show()
 
 # Great, we have a base image of some big clear text
 # Lets import pytesseract and use the dir() fundtion to get a sense of what might be some interesting
 # functions to play with
 import pytesseract
-dir(pytesseract)
+# print(dir(pytesseract))
+
+# It looks like there are just a handful of interesting functions, and I think image_to_string
+# is probably our best bet. Lets use the help() function to interrogate this a bit more
+# help(pytesseract.image_to_string))
+
+# So this function takes an image as the first parameter, then there are a bunch of optional parameters,
+# and it will return the results of the OCR. I think it's worth comparing this documentation string
+# with the documentation we were receiving from the PILLOW module. Lets run the help command on the
+# Image resize function()
+# help(Image.Image.resize)
+
+# Notice how the PILLOW function has a bit more information in it. First it's using a specific format
+# called reStructuredText, which is similar in intent to document markups such as HTML, the language of
+# the web. The intent is to embed semantics in the documentation itself. For instance, in the resize()
+# function we see the words "param size" with colons surrounding it. This allows documentation engines
+# which create web docs from source code to link the parameter to the extended docs about that parameter.
+# In this case the extended docs tell us that the size should be passed as a tuple of width and height.
+# Notice how the docs for image_to_string, for instance, indicate that there is a "lang" parameter we can
+# use, but then fail to say anything about what that parameter is for or what its format is.
+#
+# What this really means is that we need to dig deeper. Here's a quick hack if you want to look at the
+# source code of a function -- you can use the inspect getsource() command and print the results
+import inspect
+src = inspect.getsource(pytesseract.image_to_string)
+print(src)
+
+# There's actually another way in jupyter, and that's to append *two* question marks to the end of
+# a given function or module. Other editors have similar features, and is a great reason to use a
+# software development environment
+# pytesseract.image_to_string??
+
+# We can see from the source code that there really isn't much more information about what the parameters
+# are for this image_to_string function. This is because underneath the pytesseract library is calling a C++
+# library which does all of the hard work, and the author just passes through all of the calls to the
+# underlying tesseract executable. This is a common issue when working with python libraries, and it means
+# we need to do some web sleuthing in order to understand how we can interact with tesseract.
+#
+# In a case like this I just googled "tesseract command line parameters" and the first hit was what I was
+# looking for, here's the URL: https://tesseract-ocr.github.io/tessdoc/Command-Line-Usage
+#
+# This goes to a wiki page which describes how to call the tesseract executable, and as we read down we see
+# that we can actually have tesseract use multiple languages in its detection, such as English and Hindi, by
+# passing them in as "eng+hin". Very cool.
