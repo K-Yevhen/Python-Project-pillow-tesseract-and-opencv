031a7dc8dac699434ca2cd54852bb82ef9fe1b8b
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-10-11 13:19:07 +0200

    finished with the project for course
    updated py file and zipped a jupyter notebook file and html file

--- week3/project/project.py
+++ week3/project/project.py
@@ -10,6 +10,7 @@ import os
 # loading the face detection classifier
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
 # the rest is up to you!
 
 # print(help(zipfile))
@@ -17,11 +18,11 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 # writing the function to open zip files and appending to the variables
-def open_zipfile(file):
-    with ZipFile(file=file, mode="r") as zf:
-        for files in zf.infolist():
-            print("file:", files.filename)
-            Image.open(zf.open(files.filename)).show()
+# def open_zipfile(file):
+#     with ZipFile(file=file, mode="r") as zf:
+#         for files in zf.infolist():
+#             print("file:", files.filename)
+#             Image.open(zf.open(files.filename)).show()
 
 
 # print(open_zipfile("images.zip"))
@@ -50,12 +51,16 @@ def open_zipfile(file):
 
 pages_list = os.listdir('images')
 global_data_structure = []
+
 for name_file in pages_list:
-    local_list = []
-    local_list.append(name_file)
-    img = Image.open("images/"+name_file)
+    local_list = [name_file]
+    # local_list = []
+    # local_list.append(name_file)
+    # img = Image.open("images/"+name_file)
+    img = Image.open(os.path.join("images", name_file))
     local_list.append(pytesseract.image_to_string(img).replace("-\n", ""))
     global_data_structure.append(local_list)
+
 
 def finding_name(name, folder):
     for local_list in global_data_structure:
@@ -68,10 +73,14 @@ def finding_name(name, folder):
 
                 faces_each = []
 
+                # for x, y, w, h in faces:
+                #     faces_each.append(img.crop((x, y, x+w, y+w )))
+                #     # display((img.crop((x,y,x+w,y+h))).resize((110,110)))
+                #     img.crop((x, y, x + w, y + h))
+
                 for x, y, w, h in faces:
-                    faces_each.append(img.crop((x, y, x+w, y+w )))
-                    # display((img.crop((x,y,x+w,y+h))).resize((110,110)))
-                    img.crop((x, y, x + w, y + h))
+                    face_img = img.crop((x, y, x + w, y + h))
+                    faces_each.append(face_img)
 
                 contact_sheet = Image.new(img.mode, (550, 110 * int(np.ceil(len(faces_each)/5))))
                 x = 0
@@ -88,8 +97,8 @@ def finding_name(name, folder):
                         x = x + 110
 
                 contact_sheet.show()
-            except:
-                print("There weren't find any faces in that file!")
+            except Exception as e:
+                print(f"There weren't find any faces in that file {local_list[0]}! Error: {e}")
 
 
 print(finding_name("Mark", "images/"))
